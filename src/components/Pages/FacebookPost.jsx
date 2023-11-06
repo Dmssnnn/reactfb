48d8d275b7b2d744b4d746ece073537b657a4b42
Dmssnnn <129981482+Dmssnnn@users.noreply.github.com>
2023-11-06 15:18:37 +0400

    Update FacebookPost.jsx

--- src/components/Pages/FacebookPost.jsx
+++ src/components/Pages/FacebookPost.jsx
@@ -8,26 +8,17 @@ const FacebookPost = () => {
   const [submittedPost, setSubmittedPost] = useState('');
 
   const handlePostSubmit = () => {
-    setSubmittedPost(post); 
-    setPost('');
+    setSubmittedPost(post)
   };
 
   return (
     <div className="postDivcontainer">
       <div className="createPostDiv">
         <Input
-          value={post}
-          placeHolder={`What's on your mind, ${storedName}?`}
-          onChange={(e) => setPost(e.target.value)}
-          width={500}
-          height={50}
+          value={post}  placeHolder={`What's on your mind, ${storedName}?`}  onChange={(e) => setPost(e.target.value)}  width={500}  height={50}
         />
         <Button
-          onClick={handlePostSubmit}
-          Value="Submit" 
-          width={100}
-          height={50}
-          backgroundColor="black"
+          onClick={handlePostSubmit}  Value="Submit"   width={100}  height={50}  backgroundColor="black"
         />
       </div>
       <div className="postPlace">
